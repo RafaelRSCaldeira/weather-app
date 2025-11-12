d68f70c51d53a4c45870c9832cadadebb98f3767
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-11-12 03:26:13 -0300

    feat: add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-import { BasicInput } from '@/components/ui/input';
 import Title from './components/Title';
-import { PrimaryButton } from '@/components/ui/button';
 import Navbar from './components/Navbar';
 import MainWeatherInfo from './components/MainWeatherInfo';
+import SearchPlace from './components/SearchPlace';
+import ExtraWeatherInfo from './components/ExtraWeatherInfo';
+import Footer from './components/Footer';
 
 export default function Home() {
 	return (
 		<>
 			<Navbar />
-			<div className="flex flex-col items-center overflow-hidden w-[90%] mx-auto">
+			<div className="flex flex-col items-center overflow-hidden w-[90%] mx-auto gap-12">
 				<Title />
-				<BasicInput
-					id="search-bar"
-					placeholder="Search for a place..."
-					containerClassName="mt-12 w-full"
-				/>
-				<PrimaryButton className="w-full mt-4">Search</PrimaryButton>
-				<MainWeatherInfo className='mt-8'/>
+				<SearchPlace />
+				<MainWeatherInfo />
+				<ExtraWeatherInfo />
 			</div>
+			<Footer />
 		</>
 	);
 }
